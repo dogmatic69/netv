6c7c059e8f591127f7c7397616673f6220809ca8
dogmatic69 <dogmatic69@gmail.com>
2014-04-05 23:49:22 +0100

    initial code

---
+++ webroot/js/main.js
@@ -0,0 +1,31 @@
+var keyPressedArray = new Array();
+
+$(document).keydown(function(event) {
+	var keycode = event.which;
+	var isRepeat = (keyPressedArray[""+keycode] == true) ? true : false;
+	keyPressedArray[""+keycode] = true;
+
+	//event.preventDefault();
+	if (keycode == 37) {
+		fButtonPress('left', 1, isRepeat);
+	} else if (keycode == 39) {
+		fButtonPress('right', 1, isRepeat);
+	} else if (keycode == 38) {
+		fButtonPress('up', 1, isRepeat);	
+	} else if (keycode == 40) {
+		fButtonPress('down', 1, isRepeat);
+	} else if (keycode == 13) {
+		fButtonPress('center', 1, isRepeat);
+	} else if (keycode == 33) {
+		fButtonPress('cpanel', 1, isRepeat);
+	} else if (keycode == 34) {
+		fButtonPress('widget', 1, isRepeat);
+	} 
+
+	return true;
+});
+
+$(document).keyup(function(event) {
+	keyPressedArray['' + event.which] = false;
+	return true;
+});
