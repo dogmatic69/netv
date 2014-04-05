bc2dc61991de5617dfafc4983ad9aa515de74f33
dogmatic69 <dogmatic69@gmail.com>
2014-04-05 23:55:58 +0100

    add button changes

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -7,19 +7,20 @@ $(document).keydown(function(event) {
 
 	//event.preventDefault();
 	if (keycode == 37) {
-		fButtonPress('left', 1, isRepeat);
+		//fButtonPress('left', 1, isRepeat);
 	} else if (keycode == 39) {
-		fButtonPress('right', 1, isRepeat);
+		//fButtonPress('right', 1, isRepeat);
 	} else if (keycode == 38) {
-		fButtonPress('up', 1, isRepeat);	
+		//fButtonPress('up', 1, isRepeat);	
 	} else if (keycode == 40) {
-		fButtonPress('down', 1, isRepeat);
+		//fButtonPress('down', 1, isRepeat);
 	} else if (keycode == 13) {
-		fButtonPress('center', 1, isRepeat);
+		//fButtonPress('center', 1, isRepeat);
 	} else if (keycode == 33) {
-		fButtonPress('cpanel', 1, isRepeat);
+		//fButtonPress('cpanel', 1, isRepeat);
+		$('.config').toggle();
 	} else if (keycode == 34) {
-		fButtonPress('widget', 1, isRepeat);
+		//fButtonPress('widget', 1, isRepeat);
 	} 
 
 	return true;
